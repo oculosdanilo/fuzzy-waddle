9cfc5ebb902502bb9efe25bb5e62c4050a879b81
amisterdann <mrdanilob.brc@gmail.com>
2022-02-17 10:53:16 -0300

    nham

---
+++ 0216/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP</title>
+    <style>
+        *:not(body) {
+            background-color: #000;
+            font-size: 2em;
+            margin: 0;
+            padding: 0;
+            color: #fff;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    $nome = "<p align='center'>Danilo!!!</p>";
+    echo $nome;
+
+    $n1 = 1;
+    $n2 = 3;
+    echo $n1 + $n2;
+    ?>
+</body>
+</html>
